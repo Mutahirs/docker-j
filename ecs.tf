26a59077d13950d621c3f410fd42aab8f74a1c47
Mutahirs <101806805+Mutahirs@users.noreply.github.com>
2022-07-25 16:01:36 +0530

    Update ecs.tf

--- ecs.tf
+++ ecs.tf
@@ -42,7 +42,7 @@ resource "aws_ecs_service" "service" {
   }
   load_balancer {
     target_group_arn = aws_lb_target_group.lb_target_group.arn
-    container_name   = "pink-slon"
+    container_name   = "red"
     container_port   = 80
   }
   # Optional: Allow external changes without Terraform plan difference(for example ASG)
