79ef0ca33eeb62da98dab9db65ef9887723c93a4
Mutahirs <101806805+Mutahirs@users.noreply.github.com>
2022-08-21 14:27:51 +0530

    Update iam.tf

--- iam.tf
+++ iam.tf
@@ -2,19 +2,40 @@ resource "aws_iam_role" "ecs-instance-role" {
   name = "ecs-instance-role-test"
   path = "/"
 
-   assume_role_policy = <<EOF
+  assume_role_policy = <<EOF
 {
   "Version": "2008-10-17",
   "Statement": [
     {
-      "Action": "*"
-       "Resource": "*"
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": ["ec2.amazonaws.com"]
       },
       "Effect": "Allow"
     }
   ]
 }
 EOF
+}
+
+resource "aws_iam_role_policy" "test_policy" {
+  name = "test_policy"
+  role = aws_iam_role.ecs-instance-role.id
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "*",
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+    ]
+  })
 }
 
 
