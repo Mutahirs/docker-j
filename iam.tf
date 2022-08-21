aeea4e6a2b5490eb97d05407347a1db28f8453a7
Mutahirs <101806805+Mutahirs@users.noreply.github.com>
2022-08-21 17:21:36 +0530

    Update iam.tf

--- iam.tf
+++ iam.tf
@@ -18,25 +18,6 @@ resource "aws_iam_role" "ecs-instance-role" {
 EOF
 }
 
-resource "aws_iam_role_policy" "test_policy" {
-  name = "test_policy"
-  role = aws_iam_role.ecs-instance-role.id
-
-  # Terraform's "jsonencode" function converts a
-  # Terraform expression result to valid JSON syntax.
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = [
-          "*",
-        ]
-        Effect   = "Allow"
-        Resource = "*"
-      },
-    ]
-  })
-}
 
 
 resource "aws_iam_role_policy_attachment" "ecs-instance-role-attachment" {
